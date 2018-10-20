d62d9b0de0f52c8cb98975e3b9a385804b9de7f2
F4NT0 <gabriel.stundner@acad.pucrs.br>
2018-10-20 20:55:58 -0300

    filas 1

--- TAD_Filas/Classes_Explicadas/Filas.java
+++ TAD_Filas/Classes_Explicadas/Filas.java
@@ -1,10 +1,29 @@
 /**
  * Classe de Implementação de Filas
+ * Filas são um caso particular de lista no qual
+ * a movimentação acontece somente no inicio e no fim
+ * 
+ * POLITICA DE ACESSO AOS DADOS: FIFO
+ * First-In,First-Out: primeiro a entrar será o primeiro a sair
+ * Os elementos são retirados na ordem que entram
  */
- public class Filas{
-
+ public class Filas<T> implements FilaTAD<T>{
+    LinkedList lista = new LinkedList<>();
+    private int totalElementos;
+    private Nodo primeiro,ultimo;
+     
      //Método construtor
      public Filas(){
+        primeiro = null;
+        ultimo = null;
+        totalElementos = 0;
+     }
+
+     public void inserir(T elemento){
          
      }
+
+
+
+     
  }
