d681a57cb6461819a768004c7bc2eeeae16f806b
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2018-11-06 17:41:29 -0200

    arvores binarias continua

--- Arvores/arvoreBinaria.java
+++ Arvores/arvoreBinaria.java
@@ -3,12 +3,17 @@
  */
  public class arvoreBinaria<T>{
      //Variáveis
-     private Nodo raiz; //Nodo onde vai ficar armazenado qual é a Raiz
-
-
+     private Nodo<T> referenciaRaiz; //Nodo onde vai ficar armazenado qual é a Raiz
+    
+     /**
+      * Posições Possiveis de um Nodo em uma árvore binária
+      */
+     public enum NodoPosicao{
+        Esquerda,Direita;
+     }
 
      //Classe Interna do Nodo
-     public class Nodo{
+     public class Nodo<T>{
          //Referencia ao Nodo Pai do Nodo atual
          private Nodo pai;
          //Referencia ao Nodo da Esquerda
@@ -18,6 +23,14 @@
          //elemento
          private T elemento;
 
+         //Método Construtor do Nodo
+         public Nodo(T e){
+            elemento = e;
+            pai = null;
+            nodoEsquerda = null;
+            nodoDireita = null;
+         }
+         
          //Métodos do Nodo
 
          //Getters
@@ -34,11 +47,108 @@
 
      }
 
-     //Metodo Construtor da Arvore Binaria
-     public arvoreBinaria(){
-         
+     /**
+      * Método Construtor da Árvore, onde temos um vetor como Parâmetro
+      * @param vetor
+      */
+     public arvoreBinaria(T[] vetor){
+         //é criado uma Fila que vai indo armazenando os elementos em Nodos da Árvore
+         Filas<T> fila = new Fila<T>();
+         //criando um novo Nodo para armazenar o elemento da Fila
+         Nodo<T> novo = null; //Inicializado em null, porque devemos retirar os elementos do vetor
+         //criando um Nodo onde vai ficar armazenado quem é o primeiro Nodo
+         Nodo<T> primeiroNodo = null; //inicializado como null porque devemos definir quem ele é depois
+         //Criado uma variavel para definir de qual lado o Nodo irá ficar na árvore binária
+         NodoPosicao posicao = null;
+
+         //se o vetor não for zero
+         if(vetor.length > 0){
+             //pegamos o elemento do vetor e colocamos no novo Nodo
+             novo = new Nodo<T>(vetor[0]);
+             //colocamos o novo Nodo como referencia a  raiz, por ser o Nodo onde esta a raiz
+             referenciaRaiz = novo;
+             //adicionamos o valor do Nodo raiz na Fila
+             fila.inserir(referenciaRaiz.getElemento());
+             //agora definimos a posição do posição como esquerda
+             posicao = NodoPosicao.Esquerda;
+
+             //É criado um For para se criar todos os outros Nodos da Árvore
+             for(int i = 1 ; i < vetor.length ; i++){
+                 //atualizamos o novo Nodo toda vez que pegamos um valor do vetor
+                 novo = new Nodo<T>(vetor[i]);
+                 fila.inserir(novo.getElemento());//adicionamos os elementos na Fila
+                 primeiroNodo = new Nodo<T>(fila.pegar(0));//criamos o Nodo primeiro, onde fica armazenado o primeiro elemento da árvore
+                 novo.setPai(primeiro);//definimos o primeiro elemento da árvore como o pai do novo Nodo
+
+                 //utilizamos agora a posição que definimos antes(esquerda) para dizer para onde vai o novo Nodo
+                 if(posicao.equals(NodoPosicao.Esquerda)){
+                    primeiroNodo.setEsquerda(novo);//definimos o novo Nodo como o Nodo a esquerda do primeiro Nodo
+                    posicao = NodoPosicao.Direita; //definimos a posição como a direita, para onde vai o proximo Nodo
+                 }else{
+                     //se a posicao estiver como Direita, ele irá add o novo Nodo na direita do Primeiro Nodo
+                     primeiroNodo.setDireita(novo); 
+                     fila.retirar(); // é retirado da Fila o elemento que iniciou a construção da arvore
+                     posicao = NodoPosicao.Esquerda; //definimos a posicao como esquerda, para quando iniciar, o Nodo irá ser adicionado a esquerda e depois direita
+                 }
+
+             }
+
+         }
+
+
      }
 
-     //Métodos
+     //Métodos para se usar em Árvores
+
+     //MÉTODOS DE CAMINHAMENTO PELA ÁRVORE
+
+     /**
+      * Método de Caminhamento Pré-Fixado
+      * No Pré-fixado, o Nodo é visitado antes de seus Descendentes
+      * Ordem: 
+      *       1) Visita a Raiz
+      *       2) Percorre sub-árvores da Esquerda
+      *       3) Percorre sub-árvores da Direita 
+      */
+      public T[] preFixado(){
+          return vetor;//TODO: fazer implementação deste método  
+      }
+
+      /**
+       * Método de Caminhamento Pós-Fixado
+       * No Pós-Fixado, o Nodo é visitado depois de seus Descendentes
+       * Ordem:
+       *     1) Percorre a Sub-árvores da Esquerda
+       *     2) Percorre a Sub-árvores da Direita
+       *     3) Visita a Raiz
+       */
+      public T[] posFixado(){
+        return vetor;//TODO: fazer implementação deste método
+      }
+
+      /**
+       * Método de Caminhamento Central
+       * No Central, percorre as arvores da esquerda, depois a raiz e depois a arvore a direita
+       * Ordem:
+       *        1) Percorre as Sub-árvores da Esquerda
+       *        2) Visita a Raiz
+       *        3) Percorre as Sub-árvores da Direita
+       */
+      public T[] central(){
+        return vetor;//TODO: fazer implementação deste método
+      }
+
+      /**
+       * Método de Caminhamento de Largura
+       * Visita os Nodos pelos Niveis da Árvore, da esquerda para a direita
+       */
+      public T[] largura(){
+        return vetor;//TODO: fazer implementação deste método
+      }
+
+
+     //public T getElemEsquerda(T elemento){
+
+     //}
 
  }
